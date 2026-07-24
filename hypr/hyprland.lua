a6504f56b76d0c971d6eb04c98f534398678aeea
jaytheoutpatient <jaytheoutpatient@protonmail.com>
2026-07-24 16:53:22 +1000

    Update hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -22,11 +22,12 @@
 ------------------
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+
 hl.monitor({
-    output   = "DP-2",
-    mode     = "1920x1080@240",
-    position = "auto",
-    scale    = "1",
+    output   = "",
+    mode     = "",
+    position = "",
+    scale    = "",
 })
 
 
